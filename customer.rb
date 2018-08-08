186db12df059c28d63212d3fe733a11c7f357876
kaluzsikc <kaluzsikc@gmail.com>
2018-08-08 14:05:47 +0100

    Add complete MVP

--- customer.rb
+++ customer.rb
@@ -7,12 +7,19 @@ class Customer
     @wallet = wallet
   end
 
-  def buy_drink(drink_name, pub)
-    # return "we don't have that drink" if drink is not on the pub
-    # search for drink object
-    drink = pub.find_drink(drink_name)
-    # return drink if drink == drink.is_str
-    @wallet -= drink.price
+  # def buy_drink(drink, pub)
+  #   # return "we don't have that drink" if drink is not on the pub
+  #   # search for drink object
+  #   drink = pub.find_drink(drink_name)
+  #   # return drink if drink == drink.is_str
+  #   @wallet -= drink.price
+  # end
 
+
+  def buy_drink(drink, pub)
+    @wallet -= drink.price
+    pub.sell_drink(drink)
   end
+
+
 end
--- specs/customer_specs.rb
+++ specs/customer_specs.rb
@@ -8,12 +8,12 @@ require_relative("../drink")
 class CustomerTest < MiniTest::Test
 
   def setup
-    drink1 = Drink.new("Beer", 3)
-    drink2 = Drink.new("Ale", 4)
-    drink3 = Drink.new("Wine", 5)
-    drink4 = Drink.new("Vodka", 6)
+    @drink1 = Drink.new("Beer", 3)
+    @drink2 = Drink.new("Ale", 4)
+    @drink3 = Drink.new("Wine", 5)
+    @drink4 = Drink.new("Vodka", 6)
 
-    drinks = [drink1, drink2, drink3, drink4]
+    drinks = [@drink1, @drink2, @drink3, @drink4]
 
     @customer1 = Customer.new("Alvaro", 10)
     @pub = Pub.new("CodeDrinks", 300, drinks)
@@ -28,7 +28,7 @@ class CustomerTest < MiniTest::Test
   end
 
   def test_buy_drink
-    @customer1.buy_drink("Beer", @pub)
+    @customer1.buy_drink(@drink1, @pub)
     assert_equal(7, @customer1.wallet)
     assert_equal(3, @pub.drinks.count)
     assert_equal(303, @pub.till)
